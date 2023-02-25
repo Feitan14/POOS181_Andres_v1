152fabdf48fed925e4f1114785813dba8e221fb5
Feitan14 <andy28morales@gmail.com>
2023-02-24 18:25:05 -0600

    ejemplo de encapsulamiento

--- objetos.py
+++ objetos.py
@@ -14,26 +14,32 @@ espv = input("Escribe la especie del villano: ")
 nomv = input("Escribe el nombre del vilano: ")
 altv = float(input("Escribe la altura del villano: "))
 cargav = int(input("Cuantas balas se villano: "))
-
+print("")
 #3. creamos 2 objetos
+
 heroe=Personaje(esph,nomh,alth)
 villano=Personaje(espv,nomv,altv)
 
-#4. acceder a sus atributos y metodos de cada objeto
-print("")
-print("el personaje pertenece a el grupo mercenario: " + heroe.especie)
-print("el personaje se llama: " + heroe.nombre)
-print("el personaje mide: " + str(heroe.altura) + "metros")
+#Ejemplo set, esto hace que afuerzas sea lo que dice el SET
+heroe.setnombre("pepe pecas xd")
 
+#4. acceder a sus atributos y metodos de cada objeto
+
+print("")
+print("el personaje pertenece a el grupo mercenario: " +heroe.getespecie())
+print("el personaje se llama: " + heroe.getnombre())
+print("el personaje mide: " + str(heroe.getaltura()) + "metros")
 heroe.correr(True)
 heroe.lanzarGranada()
 heroe.recargararma(cargah)
+heroe.__pensar()
+print("")
 
+print("")
 print("ATRIBUTOS PERSONAJES")
-print("el personaje pertenece a la raza: " + villano.especie)
-print("el personaje se llama: " + villano.nombre)
-print("el personaje mide: " + str(villano.altura) + "metros")
-
+print("el personaje pertenece a la raza: " + villano.getespecie())
+print("el personaje se llama: " + villano.getnombre())
+print("el personaje mide: " + str(villano.getaltura()) + "metros")
 villano.correr(False)
 villano.lanzarGranada()
 villano.recargararma(cargav)
--- personaje.py
+++ personaje.py
@@ -2,15 +2,15 @@ class Personaje:
     
     #atributos del personaje
     def __init__(self,esp,nom,alt):
-        self.especie = esp
-        self.nombre = nom
-        self.altura = alt
+        self.__especie = esp
+        self.__nombre = nom
+        self.__altura = alt
     
     #metodos personaje
     def correr(self,status):
         if(status):
-            print("el personaje "+ self.nombre + " esta corriendo")
-        else:  print("el personaje "+ self.nombre + " se detuvo")
+            print("el personaje "+ self.__nombre + " esta corriendo")
+        else:  print("el personaje "+ self.__nombre + " se detuvo")
         
     def lanzarGranada(self):
         print("se lanzo granada ")
@@ -19,3 +19,30 @@ class Personaje:
         cargador = 5
         cargador = cargador + municiones
         print("el arma tiene ahora " +str(cargador)+ " balas")
+    
+    #ejemplo de metodo privado
+    #def __pensar(self):   
+        
+        
+    
+  #declaramos los getters y Setters de los atributos privados      
+    def getespecie(self):
+        return self.__especie
+    
+    def setespecie(self,esp):
+        self.__especie = esp
+########################################### 
+    
+    def getnombre(self):
+        return self.__nombre
+    
+    def setnombre(self,nom):
+        self.__nombre = nom
+###########################################
+
+    def getaltura(self):
+        return self.__altura
+    
+    def setaltura(self,alt):
+        self.__altura = alt
+###########################################
